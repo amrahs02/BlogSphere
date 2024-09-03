4f701f3a5288f656eb82f1b9514233e81ee8913d
Sandeep Sharma <sandeepsharma2183@gmail.com>
2024-09-03 13:04:06 +0530

    Added icons in buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,14 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../UserContext';
 
-
-// Determine the base URL based on the environment
 const baseURL = window.location.hostname === 'localhost'
   ? 'http://localhost:4000'
   : 'https://blog-hub-api-kow3.onrender.com';
 
 const Navbar = () => {
   const { setUserInfo, userInfo } = useContext(UserContext);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const fetchUserProfile = async () => {
@@ -35,60 +34,93 @@ const Navbar = () => {
         credentials: 'include',
         method: 'POST',
       });
-      setUserInfo(null);  // Update the UI
+      setUserInfo(null);
     } catch (error) {
       console.error('Error logging out:', error);
     }
   };
 
   return (
-    <div className="flex flex-wrap justify-between m-3 items-center p-4 bg-purple-600 text-white rounded-full shadow-lg">
-      <Link to="/" className="ml-5 text-2xl md:text-3xl font-bold hover:text-gray-200">
-        .BlogHub
-      </Link>
-      <nav className="flex items-center space-x-4 md:space-x-6 mt-2 md:mt-0">
-        {username ? (
-          <>
-            <span className="text-sm md:text-lg font-medium">
-              Welcome, {username}
-            </span>
-            <Link
-              className="flex bg-purple-700 hover:bg-purple-800 text-white font-semibold py-1 px-2 md:px-3 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-500"
-              to="/create"
-            >
-              Create Post
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" ml-1 size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-              </svg>
+    <header className="bg-purple-600 text-white shadow-lg">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <Link to="/" className="text-2xl md:text-3xl font-bold hover:text-gray-200">
+          .BlogHub
+        </Link>
+        <button
+          className="md:hidden text-white focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+            />
+          </svg>
+        </button>
+        <nav
+          className={`${isOpen ? "block" : "hidden"
+            } md:flex md:items-center md:space-x-6 transition-all duration-300`}
+        >
+          {username ? (
+            <>
+              <span className="text-lg font-medium hidden md:block">
+                Welcome, {username}
+              </span>
+              <Link
+                className="flex mt-2 md:mt-0 bg-purple-700 hover:bg-purple-800 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 transition"
+                to="/create"
+              >
+                Create Post
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
 
 
+              </Link>
+              <button
+                onClick={logout}
+                className="flex mt-2 md:mt-0 bg-purple-700 hover:bg-purple-800 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 transition"
+              >
+                Logout
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                </svg>
 
-            </Link>
-            <button
-              onClick={logout}
-              className="bg-purple-700 hover:bg-purple-800 text-white font-semibold py-1 px-2 md:px-3 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-500"
-            >
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link
-              className="bg-purple-700 hover:bg-purple-800 text-white font-semibold py-1 px-2 md:px-3 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-500"
-              to="/login"
-            >
-              Login
-            </Link>
-            <Link
-              className="bg-purple-700 hover:bg-purple-800 text-white font-semibold py-1 px-2 md:px-3 rounded-full focus:outline-none focus:ring-2 focus:ring-purple-500"
-              to="/register"
-            >
-              Register
-            </Link>
-          </>
-        )}
-      </nav>
-    </div>
+              </button>
+            </>
+          ) : (
+            <>
+              <Link
+                className="flex mt-2 md:mt-0 bg-purple-700 hover:bg-purple-800 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 transition"
+                to="/login"
+              >
+                Login
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+              </Link>
+              <Link
+                className="flex mt-2 md:mt-0 bg-purple-700 hover:bg-purple-800 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 transition"
+                to="/register"
+              >
+                Register
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                </svg>
+              </Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
   );
 };
 
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -10,6 +10,7 @@ const IndexPage = () => {
   const [posts, setPosts] = useState([]);
   const [filteredPosts, setFilteredPosts] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
   const searchInputRef = useRef(null);
 
   useEffect(() => {
@@ -18,7 +19,9 @@ const IndexPage = () => {
       .then(posts => {
         setPosts(posts);
         setFilteredPosts(posts); // Initialize filteredPosts with all posts
-      });
+        setIsLoading(false); // Data has been fetched
+      })
+      .catch(() => setIsLoading(false)); // Handle errors and stop loading
   }, []);
 
   const handleSearch = (e) => {
@@ -54,43 +57,47 @@ const IndexPage = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <form onSubmit={handleSearch} className="mb-4 flex flex-wrap justify-end">
+      <form onSubmit={handleSearch} className="mb-4 flex flex-col md:flex-row items-center justify-end space-y-2 md:space-y-0 md:space-x-2">
         <input
           type="text"
           value={searchQuery}
           onChange={handleInputChange}
           ref={searchInputRef}
           placeholder="Search Blogs... (Ctrl + K)"
-          className="w-full md:w-auto border px-2 border-gray-300 rounded-l-full focus:ring-2 focus:ring-purple-500 outline-none"
+          className="w-full md:flex-grow border px-4 py-2 border-gray-300 rounded-full focus:ring-2 focus:ring-purple-500 outline-none transition duration-200 ease-in-out"
         />
         <button
           type="submit"
-          className="w-full md:w-auto bg-purple-500 text-white px-2 py-1 rounded-r-full hover:bg-blue-600 mt-2 md:mt-0 transition-colors duration-300 ease-in-out"
+          className="flex justify-center items-center w-full md:w-auto bg-purple-500 text-white px-4 py-3 rounded-full hover:bg-purple-800 transition-colors duration-300 ease-in-out"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
             <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
           </svg>
-
         </button>
       </form>
-      <div className="flex flex-wrap gap-4">
-        <div className="flex-grow">
-          {filteredPosts.length > 0 ? (
-            filteredPosts.map(post => (
-              <Post
-                key={post._id}
-                {...post}
-              />
-            ))
-          ) : (
-            <p className="text-center text-gray-500">No posts found</p>
-          )}
+
+      {isLoading ? (
+        <p className="text-center text-gray-500">Loading...</p>
+      ) : (
+        <div className="flex flex-wrap gap-4">
+          <div className="flex-grow">
+            {filteredPosts.length > 0 ? (
+              filteredPosts.map(post => (
+                <Post
+                  key={post._id}
+                  {...post}
+                />
+              ))
+            ) : (
+              <p className="text-center text-gray-500">No posts found</p>
+            )}
+          </div>
+          <div className="w-full md:w-1/4 bg-gray-100 p-4 m-8 rounded-lg shadow-md">
+            <h2 className="text-xl font-semibold mb-4">Tags</h2>
+            <p className="text-gray-600">No Tags found</p>
+          </div>
         </div>
-        <div className="w-full md:w-1/4 bg-gray-100 p-4 m-8 rounded-lg shadow-md">
-          <h2 className="text-xl font-semibold mb-4">Tags</h2>
-          <p className="text-gray-600">No Tags found</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
