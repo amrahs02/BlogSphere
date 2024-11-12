905bf8bc12ba7163b2a2d3cf80060d8781395c5f
Sandeep Sharma <sandeepsharma2183@gmail.com>
2024-11-12 14:53:21 +0530

    fixed

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const dotenv = require("dotenv");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
--- api/models/Post.js
+++ api/models/Post.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  summary: { type: String, required: true },
-  content: { type: String, required: true },
-  cover: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-}, { timestamps: true });
+const PostSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    summary: { type: String, required: true },
+    content: { type: String, required: true },
+    cover: { type: String, required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
 
 const Post = mongoose.model("Post", PostSchema);
 module.exports = Post;
